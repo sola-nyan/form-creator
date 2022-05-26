23be13652e3f3166fc26609d4a7fc3abceb2d2af
Solanyan <solanyan.net@gmail.com>
2022-05-26 09:30:27 +0900

    add: fix typing and export method

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,16 +3,11 @@
  * @param params FormDataに変換したいオブジェクト
  * @returns FormDataインスタンス (multipart/form-data)
  */
-declare function objectToFormData(params: Record<string, object>): FormData | URLSearchParams;
+export declare function objectToFormData(params: Record<string, any>): FormData | URLSearchParams;
 /**
  * オブジェクトをURLSearchParamsに変換します.
  * @param params FormDataに変換したいオブジェクト
  * @returns URLSearchParamsインスタンス (application/x-www-form-urlencoded)
  */
-declare function objectToURLSearchParams(params: Record<string, object>): FormData | URLSearchParams;
-declare const _default: {
-    objectToFormData: typeof objectToFormData;
-    objectToURLSearchParams: typeof objectToURLSearchParams;
-};
-export default _default;
+export declare function objectToURLSearchParams(params: Record<string, any>): FormData | URLSearchParams;
 //# sourceMappingURL=index.d.ts.map
